76abca002d48e2a8a18825f6a7228dcc912c17f7
lewizxp <luizpedrosantos297@gmail.com>
2024-04-12 12:28:22 -0300

    trab

---
+++ exercicíos/Exercício 06/ex9.js
@@ -0,0 +1,8 @@
+let país = "Brasil"
+console.log (`Vivendo em ${país}`)
+if(país == "Brasil"){
+    console.log(" Você é Brasileiro")
+}
+else{
+    console.log(" Você é Estrangeiro")
+}
---
+++ exercicíos/Exercício 06/exer8.js
@@ -0,0 +1,7 @@
+let vel = 60
+console.log (`a velocidade do seu carro é de ${vel}`)
+if (vel > 60 ){
+    console.log ("Você ultrapassou a velocidade permitida. MULTADO")
+}
+
+console.log ("Dirija sempre usando cinto de segurança")
